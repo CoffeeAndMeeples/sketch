f830e637a85b36bf035c088522c7971d5fff5514
Cody Jorgensen <137858034+CoffeeAndMeeples@users.noreply.github.com>
2025-03-11 22:24:52 -0500

    Initial Setup, Create files, initial logic for grid

---
+++ script.js
@@ -0,0 +1,27 @@
+//create variable to hold number of div squares to create
+const squares = 48;
+//create container node
+const container = document.querySelector(".container")
+//loop a number of times equal to above variable-creating a square div each loop
+for(let i = 0; i < squares; i++) {
+    const newSquare = document.createElement("div");
+    //give the square the "pixel" class
+    newSquare.classList.add("pixel");
+    //add event listener to square
+    newSquare.addEventListener("mouseover", () => {
+        newSquare.classList.add("hovered");
+    })
+    //append the square to container
+    container.appendChild(newSquare);
+}
+
+//event listener for stylesheet color change
+function changeColor(newColor) {
+    const stylesheet = document.styleSheets[0]
+    const rules = stylesheet.cssRules 
+    for (let i = 0; i < rules.length; i++) {
+        if (rules[i].selectorText === ".hovered") {
+            rules[i].style.backgroundColor = newColor
+        }
+    }
+}
